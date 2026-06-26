c6681ede460f1e91e998b2d5ac37f8236c6ed4cd
fgcl135790-debug <fgcl135790@gmail.com>
2026-06-26 15:56:51 +0800

    Update engine_v2.py

--- tw_ai_monitor/core/engine_v2.py
+++ tw_ai_monitor/core/engine_v2.py
@@ -1,14 +1,23 @@
 import pandas as pd
 from indicators.ema import ema
+from indicators.vwap import vwap
+from indicators.volume import volume_ma, volume_spike
 
-class Engine:
+class EngineV2:
 
-    def build(self, prices):
+    def build(self, prices, volumes):
 
-        df = pd.DataFrame({"close": prices})
+        df = pd.DataFrame({
+            "close": prices,
+            "volume": volumes
+        })
 
         df["ema5"] = ema(df["close"], 5)
         df["ema20"] = ema(df["close"], 20)
         df["ema60"] = ema(df["close"], 60)
+
+        df["vwap"] = vwap(df)
+        df["vol_ma"] = volume_ma(df["volume"])
+        df["vol_spike"] = volume_spike(df["volume"])
 
         return df
