496363920fdd62b530a927b8146db4c08294295c
fgcl135790-debug <fgcl135790@gmail.com>
2026-06-26 15:51:30 +0800

    Update engine.py

--- tw_ai_monitor/core/engine.py
+++ tw_ai_monitor/core/engine.py
@@ -1,1 +1,14 @@
+import pandas as pd
+from indicators.ema import ema
 
+class Engine:
+
+    def build(self, prices):
+
+        df = pd.DataFrame({"close": prices})
+
+        df["ema5"] = ema(df["close"], 5)
+        df["ema20"] = ema(df["close"], 20)
+        df["ema60"] = ema(df["close"], 60)
+
+        return df
