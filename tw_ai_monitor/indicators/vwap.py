3cf73957d2c1fea83ec24ca8083eaa31ef2042cd
fgcl135790-debug <fgcl135790@gmail.com>
2026-06-26 16:04:57 +0800

    Update vwap.py

--- tw_ai_monitor/indicators/vwap.py
+++ tw_ai_monitor/indicators/vwap.py
@@ -1,8 +1,4 @@
 import pandas as pd
 
-def volume_ma(volume, n=20):
-    return volume.rolling(n).mean()
-
-def volume_spike(volume, n=20):
-    ma = volume_ma(volume, n)
-    return volume / ma
+def vwap(df):
+    return (df["close"] * df["volume"]).cumsum() / df["volume"].cumsum()
