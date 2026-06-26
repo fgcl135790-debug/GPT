e1b49cf2f5c7aa9448f4a6adb953c30dac48aa20
fgcl135790-debug <fgcl135790@gmail.com>
2026-06-26 16:47:09 +0800

    Update fugle_rest.py

--- tw_ai_monitor/data/fugle_rest.py
+++ tw_ai_monitor/data/fugle_rest.py
@@ -1,1 +1,20 @@
+import requests
 
+
+class FugleREST:
+
+    def __init__(self, api_key):
+        self.api_key = api_key
+
+    def get_price(self, symbol):
+
+        url = f"https://api.fugle.tw/marketdata/v1.0/stock/quote/{symbol}"
+
+        headers = {
+            "X-API-KEY": self.api_key
+        }
+
+        r = requests.get(url, headers=headers)
+        data = r.json()
+
+        return data["data"]["quote"]["tradePrice"]
