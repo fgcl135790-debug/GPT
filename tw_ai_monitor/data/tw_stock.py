4c5084b2a663b6bfe39f4ca7b38a0c8e510a7125
fgcl135790-debug <fgcl135790@gmail.com>
2026-06-26 15:30:59 +0800

    Update tw_stock.py

--- tw_ai_monitor/data/tw_stock.py
+++ tw_ai_monitor/data/tw_stock.py
@@ -1,1 +1,17 @@
+import requests
+import pandas as pd
 
+class TWStock:
+    def get_data(self, stock_id):
+        url = "https://api.finmindtrade.com/api/v4/data"
+        
+        params = {
+            "dataset": "TaiwanStockPrice",
+            "stock_id": stock_id,
+            "limit": 100
+        }
+
+        r = requests.get(url, params=params)
+        data = r.json()["data"]
+
+        return pd.DataFrame(data)
