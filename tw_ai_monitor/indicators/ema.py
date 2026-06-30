2cab62c41c6402b706a73d962f7f0220f00cb89a
fgcl135790-debug <fgcl135790@gmail.com>
2026-06-30 08:19:40 +0800

    Delete tw_ai_monitor directory

--- tw_ai_monitor/indicators/ema.py
+++
@@ -1,4 +0,0 @@
-import pandas as pd
-
-def ema(series, span):
-    return series.ewm(span=span).mean()
--- tw_ai_monitor/indicators/vwap.py
+++
@@ -1,4 +0,0 @@
-import pandas as pd
-
-def vwap(df):
-    return (df["close"] * df["volume"]).cumsum() / df["volume"].cumsum()
